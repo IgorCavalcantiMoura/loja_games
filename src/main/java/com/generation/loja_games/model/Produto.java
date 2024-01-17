f98b57412417997b225bf6276837486fd649dbcc
igor <igor_bolsistasoftex@procenge.com.br>
2024-01-17 16:37:10 -0300

    CRUD LOJA DE GAMES

---
+++ src/main/java/com/generation/loja_games/model/Produto.java
@@ -0,0 +1,89 @@
+package com.generation.loja_games.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O atributo Nome é obrigatório!")
+	@Size(min = 4, max = 100, message = "O atributo Nome deve conter no minimo 4 e no máximo 100 caracteres")
+	private String nome;
+	
+	@NotNull(message = "O atributo Preço é obrigatório!")
+	private double preco;
+	
+	
+	@Size(min = 4, max = 100, message = "O atributo Produtora deve conter no minimo 4 e no máximo 100 caracteres")
+	private String produtora;
+	
+	private LocalDate dataLancamento;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getProdutora() {
+		return produtora;
+	}
+
+	public void setProdutora(String produtora) {
+		this.produtora = produtora;
+	}
+
+	public LocalDate getDataLancamento() {
+		return dataLancamento;
+	}
+
+	public void setDataLancamento(LocalDate dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}
